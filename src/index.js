52b433aaa950a2d55430c6d8913a084ddc2a4dce
samo-war <samo_war@mail.ru>
2018-09-30 16:02:01 +0300

    Task 5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  let change = currency;
+
+  if (change > 10000) {
+    return { error: 'You are rich, my friend! We don\'t have so much coins for exchange' };
+  }
+
+  const moneyMap = [
+    { value: 50, name: 'H' }, { value: 25, name: 'Q' },
+    { value: 10, name: 'D' }, { value: 5, name: 'N' },
+    { value: 1, name: 'P' }
+  ];
+
+  return moneyMap.reduce((acc, cur) => {
+    const fullNumber = Math.floor(change / cur.value);
+
+    if (fullNumber > 0) {
+      acc[cur.name] = fullNumber;
+      change -= fullNumber * cur.value;
+    }
+
+    return acc;
+  }, {});
 }
